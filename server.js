3ae1a99716350536fbbe57aba4e995a96f70e73a
TheBigR <nevoroy@gmail.com>
2021-01-13 13:59:24 +0200

    adding mongo client

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3001, () => {
   console.log('listening on port 3001')
@@ -7,4 +11,7 @@ app.listen(3001, () => {
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+app.post('/tasks', (req, res) => {
+  console.log(req.body)
 })
