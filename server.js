337b30928d97739160b458c4c90e90892d3c81ce
TheBigR <nevoroy@gmail.com>
2021-01-14 19:07:49 +0200

    db connection created

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 
+const connectionString =
+  'mongodb+srv://rTodoUser:Tolik1984@royn.7svan.mongodb.net/tasks2?retryWrites=true&w=majority'
+
+MongoClient.connect(
+  connectionString,
+  {
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+  },
+)
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3001, () => {
