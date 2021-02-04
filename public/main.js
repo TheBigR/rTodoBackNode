c50fd4ca3ae29021540a064741f96bc76a434032
TheBigR <nevoroy@gmail.com>
2021-02-04 10:45:25 +0200

    adding delete

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
 window.onload = function () {
   const update = document.getElementById('update-button')
+  const reUpdate = document.getElementById('reUpdate-button')
+  const addSample = document.getElementById('add-button')
+  const deleteSample = document.getElementById('delete-button')
 
   update.addEventListener('click', (_) => {
     fetch('/tasks', {
@@ -8,6 +11,17 @@ window.onload = function () {
       body: JSON.stringify({
         name: 'Belle The First',
         task: 'prove this shit works',
+      }),
+    })
+  })
+
+  reUpdate.addEventListener('click', (_) => {
+    fetch('/tasks', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'Belle The First',
+        task: 'prove this shit still works',
       }),
     })
   })
